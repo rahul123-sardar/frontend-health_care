048711166886328751620760c258696aaf90656d
rahul123-sardar <rahulsardarr21@gmail.com>
2026-03-13 05:19:54 +0530

    first commit

---
+++ src/AddPatient.jsx
@@ -0,0 +1,216 @@
+import { useState } from "react";
+import axios from "axios";
+
+function AddPatient() {
+  const [image, setImage] = useState(null);
+  
+
+  const [formData, setFormData] = useState({
+    patientId: "",
+    name: "",
+    vitals: "",
+    billingCode: "",
+    diagnosis: "",
+    notes: ""
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+
+  //   try {
+
+  //     const res = await axios.post(
+  //       "http://localhost:5000/api/patient/save",
+  //       formData
+  //     );
+
+  //     alert(res.data.message);
+
+  //     setFormData({
+  //       patientId: "",
+  //       name: "",
+  //       vitals: "",
+  //       billingCode: "",
+  //       diagnosis: "",
+  //       notes: ""
+  //     });
+
+  //   } catch (error) {
+
+  //     console.log(error);
+  //     alert("Error saving patient");
+
+  //   }
+  // };
+
+
+  const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData();
+
+data.append("patientId", formData.patientId);
+data.append("name", formData.name);
+data.append("vitals", formData.vitals);
+data.append("billingCode", formData.billingCode);
+data.append("diagnosis", formData.diagnosis);
+data.append("notes", formData.notes);
+if (image) {
+  data.append("image", image);
+}
+
+  try {
+
+    const res = await axios.post(
+  "http://localhost:5000/api/patient/save",
+  data,
+  {
+    headers: {
+      "Content-Type": "multipart/form-data"
+    }
+  }
+);
+    
+
+    alert(res.data.message);
+
+  } catch (error) {
+  console.log("AXIOS ERROR:", error);
+  console.log("SERVER RESPONSE:", error.response?.data);
+
+  alert(error.response?.data?.message || "Server error");
+}
+};
+
+  return (
+
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+
+      <div className="bg-white p-8 rounded-lg shadow-md w-[500px]">
+
+        <h2 className="text-2xl font-bold mb-6 text-center">
+          Add Patient Record
+        </h2>
+
+        <form
+onSubmit={handleSubmit}
+encType="multipart/form-data"
+className="space-y-4"
+>
+
+          <div>
+            <label className="block text-sm font-medium">
+              Patient ID
+            </label>
+            <input
+              type="number"
+              name="patientId"
+              value={formData.patientId}
+              onChange={handleChange}
+              className="w-full border p-2 rounded"
+              required
+            />
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium">
+              Patient Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="w-full border p-2 rounded"
+              required
+            />
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium">
+              Vitals
+            </label>
+            <input
+              type="text"
+              name="vitals"
+              value={formData.vitals}
+              onChange={handleChange}
+              className="w-full border p-2 rounded"
+              placeholder="120/90, 98.6F"
+            />
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium">
+              Billing Code
+            </label>
+            <input
+              type="number"
+              name="billingCode"
+              value={formData.billingCode}
+              onChange={handleChange}
+              className="w-full border p-2 rounded"
+            />
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium">
+              Diagnosis
+            </label>
+            <input
+              type="text"
+              name="diagnosis"
+              value={formData.diagnosis}
+              onChange={handleChange}
+              className="w-full border p-2 rounded"
+            />
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium">
+              Doctor Notes
+            </label>
+            <textarea
+              name="notes"
+              value={formData.notes}
+              onChange={handleChange}
+              className="w-full border p-2 rounded"
+            />
+          </div>
+          <div>
+<label className="block text-sm font-medium">
+Patient Image
+</label>
+
+<input
+type="file"
+name="image"
+onChange={(e)=>setImage(e.target.files[0])}
+className="w-full border p-2 rounded"
+/>
+</div>
+
+          <button
+            type="submit"
+            className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700"
+          >
+            Save Patient
+          </button>
+
+        </form>
+
+      </div>
+
+    </div>
+
+  );
+}
+
+export default AddPatient;
---
+++ src/App.jsx
@@ -0,0 +1,112 @@
+import { useState } from "react";
+import axios from "axios";
+import AddPatient from "./AddPatient";
+import "./App.css";
+function App() {
+
+  const [data,setData] = useState(null);
+  const [role,setRole] = useState("");
+
+  const getPatient = async(roleName)=>{
+    setRole(roleName);
+
+    const res = await axios.get("http://localhost:5000/api/patient");
+    setData(res.data);
+  };
+
+  return (
+
+    <div className="bg-gray-100 p-8 min-h-screen main-container">
+      <AddPatient />
+
+      <div className="max-w-4xl mx-auto bg-white p-8 rounded-lg shadow card">
+
+        <h1 className="text-3xl font-bold text-blue-800 mb-6 title">
+        Secure PHI Access Simulator
+        </h1>
+
+        <div className="flex gap-4 mb-6 button-group">
+
+          <button
+          onClick={()=>getPatient("Nurse")}
+          className="bg-green-600 text-white px-4 py-2 rounded btn btn-nurse">
+          Nurse
+          </button>
+
+          <button
+          onClick={()=>getPatient("Billing")}
+          className="bg-yellow-500 text-white px-4 py-2 rounded btn btn-billing">
+          Billing Clerk
+          </button>
+
+          <button
+          onClick={()=>getPatient("Unauthorized")}
+          className="bg-red-500 text-white px-4 py-2 rounded btn btn-unauthorized">
+          Unauthorized
+          </button>
+
+        </div>
+
+       {role === "Nurse" && data && data.map((patient) => (
+
+  <div
+  key={patient._id}
+  className="bg-green-50 p-4 border border-green-400 mb-3 patient-box nurse-view">
+
+    <h2 className="font-bold">Nurse View</h2>
+   {patient.image && (
+  <img src={patient.image} width="120" />
+)}
+
+    <p>Patient ID: {patient.patientId}</p>
+    <p>Name: {patient.name}</p>
+    <p>Vitals: {patient.vitals}</p>
+    <p>Diagnosis: {patient.diagnosis}</p>
+    <p>Notes: {patient.notes}</p>
+
+  </div>
+
+))}
+
+        {role === "Billing" && data && data.map((patient) => (
+
+  <div
+  key={patient._id}
+  className="bg-yellow-50 p-4 border border-yellow-400 mb-3 patient-box billing-view">
+
+    <h2 className="font-bold">Billing Clerk View</h2>
+    {patient.image && (
+  <img src={patient.image} width="120" />
+)}
+
+    <p>Patient ID: {patient.patientId}</p>
+    <p>Name: {patient.name}</p>
+    <p>Billing Code: {patient.billingCode}</p>
+
+    <p className="text-red-600 mt-2 ">
+    Diagnosis & Notes are encrypted (Access denied)
+    </p>
+
+  </div>
+
+))}
+
+        {role==="Unauthorized" && (
+
+          <div className="bg-red-100 p-4 border border-red-400 patient-box denied">
+
+          <h2 className="font-bold text-red-700">
+          ACCESS DENIED
+          </h2>
+
+          </div>
+
+        )}
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
