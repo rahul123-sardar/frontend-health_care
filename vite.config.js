6bce1007d44404c500505004923027ce48a93969
rahul123-sardar <rahulsardarr21@gmail.com>
2026-03-13 07:34:43 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://backend-health-care-xr5d.vercel.app',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+};
