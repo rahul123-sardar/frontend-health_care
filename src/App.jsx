4b46ffc3abea4cd3dce704cfdc0e84e4704ff7f1
rahul123-sardar <rahulsardarr21@gmail.com>
2026-03-16 00:40:28 +0530

    Add some features

--- src/App.jsx
+++ src/App.jsx
@@ -89,7 +89,7 @@ function App() {
         )}
 
         {/* Patients Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <div className="">
           {!loading &&
             role &&
             role !== "Unauthorized" &&
