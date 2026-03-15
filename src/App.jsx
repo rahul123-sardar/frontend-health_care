3cb51c580afdf9cc6a9e6bfb2f016aa1778e6cff
rahul123-sardar <rahulsardarr21@gmail.com>
2026-03-16 00:31:53 +0530

    Add some features

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import AddPatient from "./AddPatient";
-import "./App.css";
+
 
 function App() {
   const [patients, setPatients] = useState([]);
